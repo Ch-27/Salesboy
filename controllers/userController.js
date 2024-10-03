ecdfd21541a9e219b94962b565aabb102e1c42bd
Ch-27 <201220015@nitdelhi.ac.in>
2024-10-03 23:29:28 +0530

    Reapply "used IP to get location"
    This reverts commit 72de83c681cbaf0a0a90930730a5e8e6e4a7ff54.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,7 @@
 import User from '../models/userModel.js';
+import { fetchLocationFromIP } from '../utils/locationUtils.js'
+
+
 
 // Get all users
 const getUsers = async (req, res) => {
@@ -10,10 +13,29 @@ const getUsers = async (req, res) => {
   }
 };
 
+
+
 // Create a new user
-const createUser = async (req, res) => {
+ const createUser = async (req, res) => {
   try {
-    const user = new User(req.body);
+    const { name, email, preferences } = req.body;
+
+    // Get user's IP address from request (assuming it's forwarded in headers)
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    // const ip="103.164.211.143"; (use your ip for maual testing)
+    // Fetch location data using the IP address
+    const location = await fetchLocationFromIP(ip);
+    // console.log('Fetched location:', location);
+    // Create a new user with location data stored in profile
+    const user = new User({
+      name,
+      email,
+      preferences,
+      profile: {
+        location: location || {},  // Save location in profile if available
+      },
+    });
+
     await user.save();
     res.status(201).json(user);
   } catch (error) {
