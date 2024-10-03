b1f88e7ee44b25c9e8c01b31e1736c7e4715e22f
Ch-27 <201220015@nitdelhi.ac.in>
2024-10-03 23:11:59 +0530

    Revert "used IP to get location"
    This reverts commit 8f3f0a2294addc37cbd3d419f4bd88ee297b9a5c.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,4 @@
 import User from '../models/userModel.js';
-import { fetchLocationFromIP } from '../utils/locationUtils.js'
-
-
 
 // Get all users
 const getUsers = async (req, res) => {
@@ -13,29 +10,10 @@ const getUsers = async (req, res) => {
   }
 };
 
-
-
 // Create a new user
- const createUser = async (req, res) => {
+const createUser = async (req, res) => {
   try {
-    const { name, email, preferences } = req.body;
-
-    // Get user's IP address from request (assuming it's forwarded in headers)
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    // const ip="103.164.211.143"; (use your ip for maual testing)
-    // Fetch location data using the IP address
-    const location = await fetchLocationFromIP(ip);
-    // console.log('Fetched location:', location);
-    // Create a new user with location data stored in profile
-    const user = new User({
-      name,
-      email,
-      preferences,
-      profile: {
-        location: location || {},  // Save location in profile if available
-      },
-    });
-
+    const user = new User(req.body);
     await user.save();
     res.status(201).json(user);
   } catch (error) {
--- models/userModel.js
+++ models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   preferences: { type: Array, default: [] },   // Stores user preferences
-  profile: {type: Object, default: {}},      // Contextual data, purchase history
+  profile: { type: Object, default: {} },       // Contextual data, purchase history
 }, { timestamps: true });
 
 export default mongoose.model('User', userSchema);
